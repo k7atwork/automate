e42390568556989b54b65e5aa949d6ac70de2795
K7 <k7atwork@users.noreply.github.com>
2017-09-29 08:44:04 +0930

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,3 @@
+# Single Click PS Login
+
+javascript:function%20E(){var%20usrid=prompt("User%20ID","a...");f0=window('dv2wrklst').document.forms[0];f0['userid'].value=usrid;f0['pwd'].value='Test@123';f0['Submit'].click();}E()
