fe41ba4f12b9137262bc60b927a19010eacd96cd
K7 <k7atwork@users.noreply.github.com>
2017-09-29 08:50:00 +0930

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
-# Single Click PS Login
+// Single Click PS Login
 
 javascript:function%20E(){var%20usrid=prompt("User%20ID","a...");f0=window('dv2wrklst').document.forms[0];f0['userid'].value=usrid;f0['pwd'].value='Test@123';f0['Submit'].click();}E()
+
+//gO-JIRA
+
+javascript:function%20E(){var%20jiraid=prompt("jira-ID","");var%20proj='';if(jiraid.indexOf("-")==-1){if(jiraid>3000){proj='HCM-';}else{proj='FA-';}}window.open('https://jira.adelaide.edu.au/browse/'+proj+jiraid);}E()
+
+// Enable Paste
+
+javascript:void(document.onpaste=null);
+
+// Enable RightClick
+
+javascript:void(document.oncontextmenu=null);
+
+// Mailto
+
+mailto:mona.sharma@letsedu.in?subject=Please%20provide%20below%20details%20for%20Digital%20Marketing%20New%20Batches&body=Name%3A%0A%0AMobile%3A%0A%0AMessage%3A
+
+// Quick Notepad in browser
+
+data:text/HTML,%20<html%20contenteditable>
+  
+// Google Bookmark
+  
+javascript:(function(){var%20a=window,b=document,c=encodeURIComponent,d=a.open("https://www.google.com/bookmarks/mark?op=edit&output=popup&bkmk="+c(b.location)+"&title="+c(b.title),"bkmk_popup","left="+((a.screenX||a.screenLeft)+10)+",top="+((a.screenY||a.screenTop)+10)+",height=510px,width=550px,resizable=1,alwaysRaised=1");a.setTimeout(function(){d.focus()},300)})();
